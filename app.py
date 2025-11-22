7d3097ebacd0f69c1b9b012dcbb1829a0e98be91
Jay-Kanakia-37 <joykanakia20061994@gmail.com>
2025-11-22 10:32:07 +0530

    linting issues fixed

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
 
+def sub(a, b): 
+    return a - b
 
 
+def mul(a, b):
+    return a * b
 
 
-
-    def sub(a,b): return a-b
-    def mul(a,b):
-        return a*b
-
-
-
-    if __name__=="__main__":
-        result1=add(4,5)
-        result2=sub(5,4)
-
-        print(result1,result2)
+if __name__ == "__main__":
+    result1 = add(4, 5)
+    result2 = sub(5, 4)
+    print(result1, result2)
