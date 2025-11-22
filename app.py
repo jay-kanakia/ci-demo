cbf396b0d0214a2e1745fab984042e0633da565d
Jay-Kanakia-37 <joykanakia20061994@gmail.com>
2025-11-22 10:11:48 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+def add(a,b):
+    return a+b
+
+
+
+
+
+
+
+    def sub(a,b): return a-b
+    def mul(a,b):
+        return a*b
+
+
+
+    if __name__=="__main__":
+        result1=add(4,5)
+        result2=sub(5,4)
+
+        print(result1,result2)
